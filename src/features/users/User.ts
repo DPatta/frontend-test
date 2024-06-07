343101c5138f26a5b5ffdb4011a92a7add1c7a2d
DPatta <105843886+DPatta@users.noreply.github.com>
2024-06-07 16:49:10 +0700

    update

--- src/features/users/User.ts
+++ src/features/users/User.ts
@@ -22,20 +22,20 @@ const userSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {
-    addUser: (state, action: PayloadAction<User>) => {
+    addUser: (state : any, action: PayloadAction<User>) => {
       state.users.push(action.payload);
       localStorage.setItem('users', JSON.stringify(state.users));
     },
-    updateUser: (state, action: PayloadAction<{ id: number; data: User }>) => {
+    updateUser: (state : any, action: PayloadAction<{ id: number; data: User }>) => {
       const { id, data } = action.payload;
-      const index = state.users.findIndex(user => user.id === id);
+      const index = state.users.findIndex((user : any) => user.id === id);
       if (index !== -1) {
         state.users[index] = data;
         localStorage.setItem('users', JSON.stringify(state.users));
       }
     },
-    deleteUser: (state, action: PayloadAction<number>) => {
-      state.users = state.users.filter(user => user.id !== action.payload);
+    deleteUser: (state : any, action: PayloadAction<number>) => {
+      state.users = state.users.filter((user : any) => user.id !== action.payload);
       localStorage.setItem('users', JSON.stringify(state.users));
     },
   },
